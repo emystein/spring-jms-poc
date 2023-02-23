dc6c5a7d3ff679581ee31e624c9721eaadc2efdc
Emiliano Menéndez <emystein@gmail.com>
2023-02-22 21:42:47 -0300

    Throw exception if message sent to test@example.com

--- src/main/java/com/hackerrank/scheduling/message/ScheduledMessageReceiver.java
+++ src/main/java/com/hackerrank/scheduling/message/ScheduledMessageReceiver.java
@@ -1,19 +1,16 @@
 package com.hackerrank.scheduling.message;
 
 import com.hackerrank.scheduling.model.MessageObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ScheduledMessageReceiver {
-    @Autowired
-    private JmsTemplate jmsTemplate;
-
     @JmsListener(destination = "info@example.com")
     public void receiveMessage(MessageObject message) {
-//        if message.getToAddress().equals("")
+        if (message.getToAddress().equals("test@example.com")) {
+            throw new RuntimeException("Message sent to test@example.com");
+        }
         System.out.println(message.getMessage());
     }
 }

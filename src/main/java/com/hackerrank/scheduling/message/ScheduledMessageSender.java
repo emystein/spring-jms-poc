2e6a7d709c85ff2090ec515b9f3152663f9d6834
Emiliano Menéndez <emystein@gmail.com>
2023-02-22 21:21:08 -0300

    Send info message every 5 seconds.

--- src/main/java/com/hackerrank/scheduling/message/ScheduledMessageSender.java
+++ src/main/java/com/hackerrank/scheduling/message/ScheduledMessageSender.java
@@ -1,12 +1,19 @@
 package com.hackerrank.scheduling.message;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ScheduledMessageSender {
+    @Autowired
+    private JmsTemplate jmsTemplate;
 
+    @Scheduled(fixedDelay = 5000)
     public void sendingInfoMessage() {
-        // send message `info` toAddress `info@example.com` at each 5 sec
+        jmsTemplate.convertAndSend("info@example.com", "info");
     }
 
     public void sendingTestMessage() {
